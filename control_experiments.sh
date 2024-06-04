9aa18be2c13c4a40890156efea4df9fdaaeb9807
Gwendls110303 <gwendls110303@yahoo.ca>
2024-06-04 11:26:20 -0600

    added cloning github repo to run experiments

--- control_experiments.sh
+++ control_experiments.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=0-2:59
-#SBATCH --array=0-2
+#SBATCH --array=4-6
 
 # SOCKS5 Proxy
 if [ "$SLURM_TMPDIR" != "" ]; then
@@ -18,21 +18,21 @@ cd $SLURM_TMPDIR
 tar -xzf classicenv.tar.gz
 ls -l
 
-echo "Copying code"
-cp ~/projects/def-mbowling/gwynetha/rcrl/dqn.py $SLURM_TMPDIR/
-cd $SLURM_TMPDIR
-ls -l
+# echo "Copying code"
+# cp ~/projects/def-mbowling/gwynetha/rcrl/dqn.py $SLURM_TMPDIR/
+# cd $SLURM_TMPDIR
+# ls -l
 
 echo "Cloning repo..."
 git config --global http.proxy 'socks5://127.0.0.1:8888'
 git clone --quiet https://github.com/swannercjj/rcrl.git $SLURM_TMPDIR/project
 
 echo "Exporting env variables"
-export PYTHONPATH=$SLURM_TMPDIR/project/src:.
+export PYTHONPATH=$SLURM_TMPDIR/project/
 export python_venv=$SLURM_TMPDIR/virtualenvs/classicenv/bin/python3.10
 echo "Running experiment..."
 
 cd $SLURM_TMPDIR/project #put project back in for cloning repo
-$python_venv dqn.py --seed $SLURM_ARRAY_TASK_ID
+$python_venv dqn.py --seed $SLURM_ARRAY_TASK_ID 
 # Don't need this for wandb
 cp -r runs ~/projects/def-mbowling/gwynetha/rcrl/control_runs
