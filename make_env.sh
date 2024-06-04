1286fc5bb6173b01b3945631be6475f6d38a9566
Jiajing Chen <jiajing8@beluga1.int.ets1.calculquebec.ca>
2024-06-04 16:34:37 -0400

    make env file

---
+++ make_env.sh
@@ -0,0 +1,23 @@
+module load python/3.11.5 StdEnv/2023 swig && \
+	ssh -q -N -T -f -D 8888 `echo $SSH_CONNECTION | cut -d " " -f 3` && \
+	export ALL_PROXY=socks5h://localhost:8888
+
+mkdir -p /tmp/jiajing/virtualenvs && \
+	cd /tmp/jiajing/virtualenvs && \
+	echo "making venv" && \
+	virtualenv --no-download pyenv && \
+	source pyenv/bin/activate && \
+	echo "activated..." && \
+	pip install requests[socks] --no-index && \
+	echo "has socks..." && \
+	pip install --no-cache-dir "gymnasium[classic-control]" "gymnasium[box2d]" numpy "stable_baselines3==2.0.0a1" tqdm tyro torch tensorboard wandb --index-url https://pypi.org/simple --extra-index-url https://download.pytorch.org/whl/cpu && \
+	echo "finished installing packages" && \
+	cd /tmp/jiajing/ && \
+	echo "zipping..." && \
+	tar -czf venv.tar.gz virtualenvs && \
+	echo "moving..." && \
+	mv venv.tar.gz ~/projects/def-mbowling/jiajing8/ && \
+	echo "cleaning up..." && \
+	rm -fr /tmp/jiajing
+
+echo "done"
