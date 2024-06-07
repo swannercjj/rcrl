27e7b3355d08d9e0a8b4a590f163cb6addbfef26
Jiajing Chen <jiajing8@beluga4.int.ets1.calculquebec.ca>
2024-06-07 16:58:29 -0400

    shell script typos

--- pull_results/slurm_pull_data.sh
+++ pull_results/slurm_pull_data.sh
@@ -3,7 +3,6 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --time=0-2:59
-#SBATCH --array=0-29
 
 # SOCKS5 Proxy
 if [ "$SLURM_TMPDIR" != "" ]; then
@@ -20,14 +19,15 @@ ls -l
 
 echo "Cloning repo..."
 git config --global http.proxy 'socks5://127.0.0.1:8888'
-git clone --quiet https://github.com/swannercjj/rcrl.git $SLURM_TMPDIR/project
+# for when branches are merged
+# git clone --quiet https://github.com/swannercjj/rcrl.git $SLURM_TMPDIR/
+git clone --quiet https://github.com/swannercjj/rcrl.git --branch comparison-check --single-branch $SLURM_TMPDIR/project
 
 echo "Exporting env variables"
 export PYTHONPATH=$SLURM_TMPDIR/project/
 export python_venv=$SLURM_TMPDIR/virtualenvs/pyenv/bin/python3.11
-echo "Running experiment..."
 
-cd $SLURM_TMPDIR/project/pull_results # put project back in for cloning repo
-git checkout comparison-check
+echo "Running experiment..."
+cd $SLURM_TMPDIR/project/pull_results
 $python_venv pull_data.py --entity_name openrlbenchmark
-# $python_venv dqn.py --seed $SLURM_ARRAY_TASK_ID --track --wandb_project_name 'Acrobot_Trials2.0' --env_id 'Acrobot-v1'
+cp -r data ~/projects/def-mbowling/jiajing8/rcrl/
