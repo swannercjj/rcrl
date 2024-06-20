32f52525c58182b3f46603eb9513670bfb8df3b5
Gwendls110303 <gwendls110303@yahoo.ca>
2024-06-20 13:46:53 -0600

    fixed the titles but need to adujust the spaces of the x axis values

--- pull_results/plot_graph.py
+++ pull_results/plot_graph.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 from dataclasses import dataclass
 import tyro
+import numpy as np
 
 
 @dataclass
@@ -16,6 +17,18 @@ if __name__=="__main__":
 
     file_path = os.path.join(f"./data/{args.hyperparam}.csv")
 
+    x_vals = {
+    'learning_rate': [1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 'Learning Rate'],
+    'buffer_size':[500,5000, 50000, 500000, 5000000 ,'Buffer Size'],
+    'gamma':[.99, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 'Gamma'],
+    'tau':[1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, "Tau"],
+    'target_network_frequency':[5, 50, 500, 5000, 50000, "Target Network Frequency"],
+    'batch_size':[16, 32, 64, 128, 256, 'Batch Size'],
+    'exploration_fraction': [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 'Exploration Fraction'],
+    'learning_starts': [10, 100, 1000, 10000, 100000, 'Learning Starts'],
+    'train_frequency': [1, 2, 4, 8, 16, "Train Frequency"]
+    }
+
     if not os.path.exists(file_path):
         print(f"Data for {args.hyerparam} does not exist.")
         exit()
@@ -28,14 +41,18 @@ if __name__=="__main__":
 
     # Plot the data
     plt.figure(figsize=(10, 6))
-
+    labels = x_vals[args.hyperparam][:-1]
+    
     # Looping through environments
     for env_id in grouped_df['env_id'].unique():
         env_data = grouped_df[grouped_df['env_id'] == env_id]
         plt.plot(env_data[args.hyperparam], env_data['mean_episodic_return'], marker='o', label=f'{env_id}')
 
+    plt.xticks(range(len(x_vals[args.hyperparam])-1), sorted(labels))
+    
+    plt.title("Mean Episodic Return of Varying "+x_vals[args.hyperparam][-1])
     plt.xlabel(args.hyperparam)
-    plt.ylabel('mean_episodic_return')
+    plt.ylabel('Mean Episodic Return')
     plt.legend()
     plt.grid(True)
 
