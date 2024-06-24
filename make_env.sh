1383cd5970ad212975dda65264ac9ed70808069d
Gwendls110303 <gwendls110303@yahoo.ca>
2024-06-24 16:17:12 -0600

    loaded opencv

--- make_env.sh
+++ make_env.sh
@@ -1,4 +1,4 @@
-module load python/3.11.5 StdEnv/2023 swig && \
+module load python/3.11 StdEnv gcc opencv/4.8.1 swig && \
 	ssh -q -N -T -f -D 8888 `echo $SSH_CONNECTION | cut -d " " -f 3` && \
 	export ALL_PROXY=socks5h://localhost:8888
 	#export ALL_PROXY="https://localhost:8888/"
@@ -6,16 +6,15 @@ module load python/3.11.5 StdEnv/2023 swig && \
 mkdir -p /tmp/gwen/virtualenvs && \ 
 	cd /tmp/gwen/virtualenvs && \
 	echo "making venv" && \
-	virtualenv --no-download pyenv && \
+	virtualenv pyenv && \
 	source pyenv/bin/activate && \
 	echo "activated..." && \
 	pip install 'requests[socks]' --no-index && \
-	pip install pysocks --no-index && \
 	echo "has socks..." && \
 	pip install --no-cache-dir "gymnasium[atari]" && \
 	pip install --no-cache-dir autorom && \
 	AutoROM -y && \
-	pip install --no-cache-dir "gymnasium[classic-control]" "gymnasium[box2d]" numpy "stable_baselines3==2.0.0a1" tqdm tyro torch tensorboard wandb --index-url https://pypi.org/simple --extra-index-url https://download.pytorch.org/whl/cpu && \
+	pip install --no-cache-dir gymnasium "gymnasium[classic-control]" "gymnasium[box2d]" numpy "stable_baselines3==2.0.0a1" tqdm tyro torch tensorboard wandb --index-url https://pypi.org/simple --extra-index-url https://download.pytorch.org/whl/cpu && \
 	echo "finished installing packages" && \
 	cd /tmp/gwen/ && \
 	echo "zipping..." && \
@@ -29,4 +28,3 @@ mkdir -p /tmp/gwen/virtualenvs && \
 
 echo "done"
 
-# added "gymnasium[atari,accept-rom-license]"
