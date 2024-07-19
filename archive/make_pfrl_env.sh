ce17b6b544430b9c96800b35e7fd0c9d7c6a9a2f
Gwendls110303 <gwendls110303@yahoo.ca>
2024-07-19 14:36:25 -0400

    better pfrl make_env

---
+++ archive/make_pfrl_env.sh
@@ -0,0 +1,30 @@
+module load python/3.11 StdEnv/2023 gcc opencv/4.8.1 swig && \
+	ssh -q -N -T -f -D 8888 `echo $SSH_CONNECTION | cut -d " " -f 3` && \
+	export ALL_PROXY=socks5h://localhost:8888
+	#export ALL_PROXY="https://localhost:8888/"
+
+mkdir -p /tmp/gwen/virtualenvs && \
+	cd /tmp/gwen/virtualenvs && \
+	echo "making venv" && \
+	virtualenv pyenv && \
+	source pyenv/bin/activate && \
+	echo "activated..." && \
+	pip install 'requests[socks]' --no-index && \
+	pip install --no-cache-dir filelock pillow autorom gymnasium "gymnasium[classic-control,box2d,atari,other]" "numpy<2" "stable_baselines3==2.0.0a1" tqdm tyro torch tensorboard wandb --index-url https://pypi.org/simple && \
+	pip install pfrl@git+https://github.com/prabhatnagarajan/pfrl@gymnasium_support && \
+	AutoROM -y && \ 
+	echo "finished installing packages" && \
+	pip list && \
+
+	cd /tmp/gwen/ && \
+	echo "zipping..." && \
+
+	tar -czf pfrlenv.tar.gz virtualenvs && \
+	echo "moving..." && \
+	mv pfrlenv.tar.gz ~/projects/def-mbowling/gwynetha/ && \
+
+	echo "cleaning up..." && \
+	rm -fr /tmp/gwen
+
+echo "done"
+
