e4e9c078d33ee4f1a271bd25d69eb6ad5d81c270
Jessica Chen <jchen@Jessicas-MacBook-Pro.local>
2024-07-29 14:47:45 -0600

    train script

--- pfrl/pfrl_run_online.sh
+++ pfrl/pfrl_run_online.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=def-mbowling
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=10G
-#SBATCH --time=1-0
+#SBATCH --time=36:00:00
 #SBATCH --array=1-50
 
 if [ "$SLURM_TMPDIR" != "" ]; then
@@ -15,14 +15,14 @@ fi
 module load python/3.11 StdEnv/2023 gcc opencv/4.8.1 swig 
 
 echo "Copying virtualenv..."
-cp ~/projects/def-mbowling/gwynetha/pfrlenv.tar.gz $SLURM_TMPDIR/
+cp ~/projects/def-mbowling/jiajing8/pfrlenv.tar.gz $SLURM_TMPDIR/
 cd $SLURM_TMPDIR
 tar -xzf pfrlenv.tar.gz
 ls -l
 
 echo "Cloning repo..."
 git config --global http.proxy 'socks5://127.0.0.1:8888'
-git clone --quiet https://github.com/swannercjj/rcrl.git $SLURM_TMPDIR/project
+git clone --quiet -b learn-action-repeats https://github.com/swannercjj/rcrl.git $SLURM_TMPDIR/project
 
 export python_venv=$SLURM_TMPDIR/virtualenvs/pyenv/bin/python3.11
 
@@ -31,8 +31,8 @@ PYTHONPATH=$SLURM_TMPDIR/project/:$PYTHONPATH $python_venv project/pfrl/train_dq
     --env "ALE/Pong-v5" \
     --seed $SLURM_ARRAY_TASK_ID \
     --track \
-    --wandb_project_name 'Pfrl_Pong_Paper_0m' \
-    --sanity_mod 1000000 \
-    --steps 10000000
+    --wandb_project_name 'Pfrl_Learn_AR_Pong' \
+    --sanity_mod 1_000_000 \
+    --steps 10_000_000
 
-cp -r results '/home/gwynetha/projects/def-mbowling/gwynetha/rcrl/pfrl/results'
+cp -r results '/home/jiajing8/scratch/rcrl/pfrl/results'
