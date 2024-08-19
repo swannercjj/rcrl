2da7cbdaf67232fbb6cbc31cf878bb78b684d596
Gwendls110303 <gwendls110303@yahoo.ca>
2024-08-19 11:41:30 -0600

    Pulling results from wandb

--- pfrl/pull_results/pull_pfrl.py
+++ pfrl/pull_results/pull_pfrl.py
@@ -17,17 +17,20 @@ value = {baseline value like 1 or AR}
 
 ex) mode_0_val_1_seed_1.csv or mode_1_AR_seed_1.csv
 
+TODO: 
+- fix the different mode csv names
+- fix the data_mode (table vs graphs)
 '''
 
 @dataclass
 class Args:
     entity_name: str = "rcrl"
     """Wandb workspace name."""
-    project_name: str = "Learn_AR"
+    project_name: str = "Learn_AR_2.0"
     """Project to pull from."""
-    data_dir: str = "./data/"
+    data_dir: str = "./data_AR/"
     """The location to store cached wandb data and downloaded data."""
-    data_mode: str = "graph" # "mean"
+    data_mode: str = "graphing" # "table"
 
 
 def cache_runs(project_name, entity_name, data_dir):
@@ -59,12 +62,14 @@ def cache_runs(project_name, entity_name, data_dir):
     with ThreadPoolExecutor(max_workers=8) as executor:
         executor.map(process_run, (enumerate(runs)))
 
+
 def save_graph_data(data, config, data_dir):
-    # mode_1_AR_seed_1.csv 
+    # saves a csv file for graphing the episodic return
     data = data[~data['charts/episodic_return'].isnull()]
-    file_path = os.path.join(data_dir, config.get('env')[4:])
+    file_path = os.path.join(data_dir, config.get('env')[4:],'graphing')
     if not os.path.exists(file_path):
         os.makedirs(file_path)
+
     if config.get('mode'): #AR learning
         file_path = os.path.join(file_path,'mode_1_AR_seed_'+str(config.get('seed'))+'.csv')
     else: # baseline const
@@ -72,40 +77,49 @@ def save_graph_data(data, config, data_dir):
     data.to_csv(file_path)
 
 
-def extract_data(project_name, entity_name, data_dir):
-    if args.data_mode == 'mean':
-        df = pd.DataFrame(columns=['env', 'seed', 'mean_episodic_return'])
+def save_table_data(df, config, data_dir):
+    # saves a csv file for making mean comparisons
+    base_path = os.path.join(data_dir, config.get('env')[4:],'tables')
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+
+    #AR learning
+    file_path = os.path.join(base_path,'mode_1_AR.csv')
+    df.query('mode==1').to_csv(file_path) # all the AR data
+    # baseline const
+    for n in df.query('mode==0')['action_repeat_n'].unique(): # different baselines
+        file_path = os.path.join(base_path,'mode_0_base_'+str(n)+'.csv')
+        df.query(f'mode==0 and action_repeat_n=={n}').to_csv(file_path)
+
+
+def extract_data(project_name, entity_name, data_dir, data_mode):
+    if data_mode == 'table':
+        df = pd.DataFrame(columns=['env', 'seed', 'mean_episodic_return', 'mode', 'action_repeat_n'])
     api = wandb.Api(timeout=20)
     runs = api.runs(f"{entity_name}/{project_name}")
     for run in runs:
         cache_path = os.path.join(data_dir, f"wandb_cache/{run.id}.pkl")
-        if run.state != "crashed":
+        if run.state != "finished":
             continue
         config = {k:v.get('value') for k, v in json.loads(run.json_config).items()}
-        # if config.get('mode') != 0: #only want mode 0/ const repeats
-        #     continue
         
         if os.path.exists(cache_path):
             run_data = pd.read_pickle(cache_path)
         else:
-            run_data = run.history()
+            run_data = run.scan_history() # run.history() just the last 500 vs run.scan_history() should give all
             run_data.to_pickle(cache_path)
-    
-        print(config)
 
         data = run_data[['charts/episodic_return', 'global_step']]
 
-        if args.data_mode == "mean": # for replicating Marlos paper format, evaluate on the last 100 episodes
+        if data_mode == "table": # for replicating Marlos paper format, evaluate on the last 100 episodes
             dic = dict(data[~data['charts/episodic_return'].isnull()][-100:].mean())
-            df.loc[len(df)] = [config.get('env'), config.get('seed'), dic.get('charts/episodic_return')]
+            df.loc[len(df)] = [config.get('env'), config.get('seed'), dic.get('charts/episodic_return'),config.get('mode'),config.get('action_repeat_n')]
         else: # making action vs decision graph
-            if config.get('mode')==1:
-                save_graph_data(data, config, data_dir)
+            save_graph_data(data, config, data_dir)
         print(f"Data saved for run {run.id} seed {config.get('seed')}")
 
-    if args.data_mode == "mean":
-        file_path = os.path.join(data_dir,config.get('env'), args.data_name+'.csv')
-        df.to_csv(file_path)
+    if data_mode == "table":
+        save_table_data(df,config,data_dir)
 
 
 if __name__=="__main__":
@@ -113,8 +127,9 @@ if __name__=="__main__":
     args = tyro.cli(Args)
 
     # Define where to store the files
-    if not os.path.exists(args.data_dir):
-        os.makedirs(args.data_dir)
+    folder = os.path.join(args.data_dir, args.project_name)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
 
-    cache_runs(args.project_name, args.entity_name, args.data_dir)
-    extract_data(args.project_name, args.entity_name, args.data_dir)
+    cache_runs(args.project_name, args.entity_name, folder)
+    extract_data(args.project_name, args.entity_name, folder,args.data_mode)
